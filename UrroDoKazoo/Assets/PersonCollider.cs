aff4844d63f58d4464f65f07bd3eab594f70e284
Michelle Beadle <michellesantiagobeadle@hotmail.com>
2018-01-27 17:57:14 -0200

    Player colidindo com coisas

---
+++ UrroDoKazoo/Assets/PersonCollider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonCollider : MonoBehaviour {
+
+	void Start(){
+		Debug.Log ("lsfdgsygs");
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Entrou: " + gameObject.name + " + " + other.name);
+	}
+
+	void OnTriggerStay(Collider other) {
+
+	}
+
+	void OnTriggerExit(Collider other) {
+
+	}
+}
