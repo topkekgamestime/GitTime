517c3d3154a3f85f49639b66611ee45dbcb7bc1e
Daniel Oliveira <thedannyvegas@gmail.com>
2018-01-28 15:34:52 -0200

    Cena Splash, já linkada, com vídeo da logo

---
+++ UrroDoKazoo/Assets/SplashToStart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashToStart : MonoBehaviour {
+
+	public AudioSource audio;
+
+	public void Play()
+	{
+		SceneManager.LoadScene (0);
+	}
+
+	public void Audio()
+	{
+		audio.Play();
+	}
+
+}
