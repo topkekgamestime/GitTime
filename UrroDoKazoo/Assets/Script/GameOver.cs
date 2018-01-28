0a5015b5f9aa28e1c3b3d36704a7df946d57f82b
Matheus L. Homsi <mlhomsi@gmail.com>
2018-01-28 17:09:21 -0200

    no message

--- UrroDoKazoo/Assets/Script/GameOver.cs
+++ UrroDoKazoo/Assets/Script/GameOver.cs
@@ -5,14 +5,21 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class GameOver : MonoBehaviour {
 
+	public Text pontuacao;
+
+	public HUDManamegent mgn;
+
 	void Start () {
+		pontuacao.text = "0";
+		pontuacao.text = mgn.money.ToString();
+
 
 	}
 
 	void Update () {
 		if(Input.GetKeyDown("space"))
 			{
-				SceneManager.LoadScene (0);
+				SceneManager.LoadScene (1);
 			}
 	}
 }
