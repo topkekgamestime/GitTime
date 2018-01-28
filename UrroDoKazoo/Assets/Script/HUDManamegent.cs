20a606c601ba96a7416bb911d2e8330e353165a4
Matheus L. Homsi <mlhomsi@gmail.com>
2018-01-28 06:52:51 -0200

    Atributos 100%

---
+++ UrroDoKazoo/Assets/Script/HUDManamegent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManamegent : MonoBehaviour {
+
+	[Range(0,100)]
+	public int x;
+	[Range(0,100)]
+	public int y;
+	[Range(0,100)]
+	public int z;
+
+	public Slider Fofo;
+	public Slider Humor;
+	public Slider Treta;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Fofo.value = x;
+		Humor.value = y;
+		Treta.value = z;
+
+
+	}
+}
