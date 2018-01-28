266e1e80029aff9e7b58f51dc01f9c3617401c4c
Michelle Beadle <michellesantiagobeadle@hotmail.com>
2018-01-27 22:20:13 -0200

    Mais coisas do homsi

---
+++ UrroDoKazoo/Assets/Script/IsVisible.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsVisible : MonoBehaviour {
+
+
+	void Update()
+	{
+		if (GetComponent<Renderer>().IsVisibleFrom(Camera.main)) 
+			Debug.Log("Hahaha");
+		else Debug.Log("Not visible");
+	}
+}
