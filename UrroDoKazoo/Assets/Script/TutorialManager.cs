7929764f865cf481abfe50f0f0a4277317959b6b
Homsi <mlhomsi@gmail.com>
2018-01-30 01:22:14 -0200

    Revert "Bug Fixes Part 1"
    This reverts commit 1f67427b75fa20c1253e81cd8f32b74e023a7ef0.

--- UrroDoKazoo/Assets/Script/TutorialManager.cs
+++ UrroDoKazoo/Assets/Script/TutorialManager.cs
@@ -5,24 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class TutorialManager : MonoBehaviour {
 
-    public GameObject gravando;
-
-
 	public void Play()
 	{
-        StartCoroutine(Gravando());
+		SceneManager.LoadScene (2);
 	}
 
-
-    IEnumerator Gravando()
-    {
-        {
-            yield return new WaitForSecondsRealtime(0.5f);
-            gravando.SetActive(true);
-           // yield return new WaitForSecondsRealtime(1.5f);
-            Debug.Log("Cabou");
-            SceneManager.LoadScene(2);
-
-        }
-    }
 }
