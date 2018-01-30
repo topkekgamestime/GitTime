1f67427b75fa20c1253e81cd8f32b74e023a7ef0
Homsi <mlhomsi@gmail.com>
2018-01-30 01:17:14 -0200

    Bug Fixes Part 1

--- UrroDoKazoo/Assets/Script/TutorialManager.cs
+++ UrroDoKazoo/Assets/Script/TutorialManager.cs
@@ -5,9 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class TutorialManager : MonoBehaviour {
 
+    public GameObject gravando;
+
+
 	public void Play()
 	{
-		SceneManager.LoadScene (2);
+        StartCoroutine(Gravando());
 	}
 
+
+    IEnumerator Gravando()
+    {
+        {
+            yield return new WaitForSecondsRealtime(0.5f);
+            gravando.SetActive(true);
+           // yield return new WaitForSecondsRealtime(1.5f);
+            Debug.Log("Cabou");
+            SceneManager.LoadScene(2);
+
+        }
+    }
 }
